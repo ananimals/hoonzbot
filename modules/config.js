a88cf64771ab2c8e014c597f97f4e131ba96a300
RevanAce <jelmer.revanace@gmail.com>
2015-06-21 15:06:18 +0000

    Lets continue

--- modules/config.js
+++ modules/config.js
@@ -1,15 +1,19 @@
 module.exports = function(data){
     var bot = data.bot;
-    
+
     var config = {};
-    
+
     config.load = function(){
         this.cfg = bot.load_json('config');
     };
     
+    config.unload = function(){
+        this.cfg = null;
+    };
+
     config.get = function(module, key){
         return this.cfg[module][key];
     };
-    
+
     return config;
 };
